ce78094b6a840c0bbd562f8cb5a3652eb0773b56
s1288295 <s1288295@cssemaclin24.monmouth.edu>
2020-02-27 15:00:12 -0500

    Grocery2

---
+++ GroceryCouponVersion2/src/GroceryDiscountVersion2.java
@@ -0,0 +1,61 @@
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
+public class GroceryDiscountVersion2 {
+	private static DecimalFormat df = new DecimalFormat("0.00");
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner sc = new Scanner(System.in);
+
+		df.setMaximumFractionDigits(2);
+		final double discountEight = 0.08;
+		final double discountTen = 0.10;
+		final double discountTwelve = 0.12;
+		final double discountFourteen = 0.14;
+		double discount = 0;
+		double price = 1;
+
+		System.out.println("Please enter the cost of your groceries or 0 to end: ");
+		
+						
+
+		while (sc.hasNextDouble()) {
+			price = sc.nextDouble();
+			
+
+			if (price == 0) {
+				break;
+			}
+			
+
+			if (price < 10 || price > 1000)
+				System.out.println("Error not in range");
+
+			else if (price < 10.00) {
+				System.out.println("You are not eligible for a coupon at this time");
+			} else if (price <= 60.00) {
+				discount = price * discountEight;
+				System.out.println("You win a discount coupon of " + df.format(discount) + " ("
+						+ df.format(discountEight * 100) + "% of your purchase)");
+			} else if (price <= 150.00) {
+				discount = price * discountTen;
+				System.out.println("You win a discount coupon of " + df.format(discount) + " ("
+						+ df.format(discountTen * 100) + "% of your purchase)");
+			} else if (price <= 210.00) {
+				discount = price * discountTwelve;
+				System.out.println("You win a discount coupon of " + df.format(discount) + " ("
+						+ df.format(discountTwelve * 100) + "% of your purchase)");
+			} else {
+				discount = price * discountFourteen;
+				System.out.println("You win a discount coupon of " + df.format(discount) + " ("
+						+ df.format(discountFourteen * 100) + "% of your purchase)");
+				
+			
+			}
+
+		}
+
+	}
+}
